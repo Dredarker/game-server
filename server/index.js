ee22beb4df9312b27575b954d45bde00c6e07f40
Ilya <141645849+Dredarker@users.noreply.github.com>
2026-04-22 15:09:06 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,14 +10,19 @@ const bannedIps = new Set([
   "123.123.123.123"
 ]);
 
+let HTMLclient;
+fetch('client/index.html')
+  .then((response) => {HTMLclient = response.text()})
+  .catch(error => console.error('Ошибка загрузки клиента:', error));
+
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Server is running");
+    res.end(HTMLclient);
   }
   if (req.url === "/healthz") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("I'm live");
+    res.end("200 OK");
   }
 });
 const wss = new WebSocket.Server({ server });
@@ -26,6 +31,89 @@ console.log(`WebSocket server started on port ${PORT}`);
 
 const clients = new Map();
 
+// game
+console.log("Initializating the game");
+
+const gravity = 0.5;
+
+const objects = new Map();
+objects.set("bottom", new Obj(-5000, 100, 10000, 100, "", "static"));
+objects.set("platform", new Obj(-500, 0, 100, 100, "", "static"));
+
+function update() {
+	objects.forEach((obj, name) => {
+		if (obj.mode === "dynamic") obj.vy += gravity;
+		
+		if (
+			obj.mode === "dynamic" ||
+			obj.mode === "kinetic"
+		) {
+			obj.x += obj.vx;
+			obj.y += obj.vy;
+		}
+
+		objects.forEach((obj2, name) => {
+			let obj1 = obj;
+			if (obj1 == obj2) return;
+			if (obj.mode == "static") return;
+
+			objRealX1 = obj1.x+obj1.width/2;
+			objRealY1 = obj1.y+obj1.height/2;
+			objRealX2 = obj2.x+obj2.width/2;
+			objRealY2 = obj2.y+obj2.height/2;
+			if (obj.type === "player") obj.onGround = false;
+			if (objInRegion(obj1, obj2.x, obj2.y, obj2.width, obj2.height)) {
+				if (objInRegion(obj1, obj2.x+5, obj2.y, obj2.width-10, obj2.height/2)) {
+					obj1.vy = 0;
+					obj1.y = obj2.y - obj1.height;
+					obj1.onGround = true;
+				} else if (objInRegion(obj1, obj2.x+5, obj2.y+obj2.height/2, obj2.width-10, obj2.height/2)) {
+					obj1.vy = 0;
+					obj1.y = obj2.y + obj2.height + obj1.height;
+				}
+				if (objInRegion(obj1, obj2.x, obj2.y+5, obj2.width/2, obj2.height-10)) {
+					obj1.vx = 0;
+					obj1.x = obj2.x - obj1.width;
+				} else if (objInRegion(obj1, obj2.x+obj2.width/2, obj2.y+5, obj2.width/2, obj2.height-10)) {
+					obj1.vx = 0;
+					obj1.x = obj2.x + obj2.width;
+				}
+			}
+		});
+	});
+}
+
+function objInRegion(obj, x, y, width, height) {
+	return (
+		obj.x < x + width &&
+		obj.x + obj.width > x &&
+		obj.y < y + height &&
+		obj.y + obj.height > y
+	)
+}
+
+function Obj(x, y, width, height, mode, type) {
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.vx = 0;
+	this.vy = 0;
+	this.mode = mode;
+	this.type = type;
+}
+
+function Player(speed, jumpPower, obj) {
+	for (let i in obj) {
+		this[i] = obj[i]
+	}
+	this.speed = speed;
+	this.jumpPower = jumpPower;
+}
+
+console.log("The game was successful initializated");
+
+// server
 wss.on("connection", (ws, req) => {
   const ip =
     req.headers["x-forwarded-for"]?.split(",")[0].trim() ||
@@ -80,6 +168,24 @@ wss.on("connection", (ws, req) => {
       return;
     }
 
+    if (data.type === "sync") {
+      for (const [id, clientData] of clients.entries()) {
+        const client = clientData.ws;
+        if (client.readyState === WebSocket.OPEN) {
+          objects.forEach((obj, name) => {
+		      if (obj.type === "player") {
+			      if (keys["KeyA"]) obj.vx += -obj.speed * (0.15 + obj.onGround);
+	      		else if (keys["KeyD"]) obj.vx += obj.speed * (0.15 + obj.onGround);
+	      		obj.vx = obj.vx * (obj.onGround ? 0.8 : 1);
+	      		if (keys["Space"] && obj.onGround) {
+	      			obj.vy = obj.jumpPower;
+		      		obj.onGround = false;
+		      	}
+	      	}
+        }
+      }
+    }
+
     if (data.type === "message") {
       for (const [id, clientData] of clients.entries()) {
         const client = clientData.ws;
@@ -91,19 +197,6 @@ wss.on("connection", (ws, req) => {
             ip: (clientData.ip == adminIp ? ip : "none")
           }));
         }
-      }
-    }
-
-    if (data.type === "message_private") {
-      const target = clients.get(data.to);
-
-      if (target && target.ws.readyState === WebSocket.OPEN) {
-        target.ws.send(JSON.stringify({
-          type: "message",
-          from: clientId,
-          text: data.text,
-          ip: (clientData.ip == adminIp ? ip : "none")
-        }));
       }
     }
 
@@ -142,5 +235,5 @@ wss.on("connection", (ws, req) => {
 });
 
 server.listen(PORT, () => {
-  console.log("Server running on port ", PORT);
+  console.log("HTTP Server running on port ", PORT);
 });
