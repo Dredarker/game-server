ee81a60917bdaa1698f359597e2ffe3f9bdd97e4
Ilya <141645849+Dredarker@users.noreply.github.com>
2026-04-25 16:25:02 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -227,19 +227,19 @@ wss.on("connection", (ws, req) => {
         if (client.readyState === WebSocket.OPEN) {
           objects.forEach((obj, objId) => {
 						if (
-							objId !== wsId ||
-							obj.type !== "player"
-						) break;
+							objId === wsId ||
+							obj.type === "player"
+						) {
+							let keys = data.keys;
+							let tmpspeed = obj.speed * (obj.onGround ? 1 : 0.1);
 
-						let keys = data.keys;
-						let tmpspeed = obj.speed * (obj.onGround ? 1 : 0.1);
-
-						if (keys["KeyA"]) obj.vx += -tmpspeed;
-	      		else if (keys["KeyD"]) obj.vx += tmpspeed;
-	      		if (keys["Space"] && obj.onGround) {
-	      			obj.vy = obj.jumpPower;
-		      		obj.onGround = false;
-		    		}
+							if (keys["KeyA"]) obj.vx += -tmpspeed;
+	      			else if (keys["KeyD"]) obj.vx += tmpspeed;
+	      			if (keys["Space"] && obj.onGround) {
+	      				obj.vy = obj.jumpPower;
+		      			obj.onGround = false;
+		    			}
+						}
 		  		});
         }
       }
