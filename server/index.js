207ba1d2010bcf2db4c31bdaf7b5038f4cd92686
Ilya <141645849+Dredarker@users.noreply.github.com>
2026-05-06 02:06:39 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -212,7 +212,7 @@ function server_sync() {
   	if (client.readyState === WebSocket.OPEN) {
 			client.send(JSON.stringify({
 				type: "sync",
-				world: objectsForClient.entries(),
+				world: Object.fromEntries(objectsForClient.entries()),
 			}));
 	  }
 	}
